db80c874e92847a260e4ddec344952d7d125cdcb
EngFred <engfred88@gmail.com>
2026-02-08 20:59:32 +0300

    updated the frontend app url in the backend CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ import { notFoundMiddleware } from './middlewares/not-found.middleware';
 const app = express();
 
 app.use(cors({
-    origin: 'https://full-stack-chat-application-engineer-freds-projects.vercel.app',
+    origin: 'full-stack-chat-application-pi.vercel.app',
     credentials: true
 }));
 app.use(express.json());
